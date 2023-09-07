1f7118bfcc0c45c856e502e087e7b2dcf7ef1fae
qkrtnqlspsb <bp1211@naver.com>
2023-09-07 15:38:16 +0900

    star

--- KNU-Fall23-C-1/KNU-Fall23-C-1.c
+++ KNU-Fall23-C-1/KNU-Fall23-C-1.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-
 int main()
 {
-	printf("Hello world\n");
+	int a;
+	scanf_s("%d", &a);
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = i; j <= a; j++)
+		{
+			printf(" ");
+		}
+		for (int j = 1; j <= i * 2 - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
 	return 0;
 }
