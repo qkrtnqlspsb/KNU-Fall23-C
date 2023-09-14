79082f51cf6fb1e1936e6d4f76a9a4279d99eb32
qkrtnqlspsb <144311039+qkrtnqlspsb@users.noreply.github.com>
2023-09-14 16:26:59 +0900

    Update lecture9.c

--- Project2/lecture9.c
+++ Project2/lecture9.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void)
@@ -15,21 +14,4 @@ int main(void)
 	printf("%d\n", hap);
 	return 0;
 }
-=======
-#include <stdio.h>
 
-int main(void)
-{
-	int count, hap = 0;
-
-	count = 1;
-	while (count <= 10)
-	{
-		hap += count;
-		printf("hap=%d, count=%d\n", hap, count);
-		count++;
-	}
-	printf("%d\n", hap);
-	return 0;
-}
->>>>>>> 576ae4c9d3a84a6b179d5619ae39627c4a90abe6
