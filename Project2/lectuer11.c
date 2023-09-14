f9352aa7b789f43b1b0b56f3a1fa2991a8c518b5
qkrtnqlspsb <bp1211@naver.com>
2023-09-14 16:28:56 +0900

    lecture11

---
+++ Project2/lectuer11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < i; j++) {
+			printf("   ");
+		}
+		if (i % 2 == 0) printf("| 0 |");
+		else printf("| X |");
+		printf("\n-----------------\n");
+	}
+	return 0;
+}
