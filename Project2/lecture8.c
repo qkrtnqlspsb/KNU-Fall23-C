962c9e6a7448fc6e86b2b86c76ced1a5906dd10c
qkrtnqlspsb <144311039+qkrtnqlspsb@users.noreply.github.com>
2023-09-14 16:14:36 +0900

    Update lecture8.c

--- Project2/lecture8.c
+++ Project2/lecture8.c
@@ -7,7 +7,7 @@ int main(void)
 	for (count = 1; count <= 10; count++)
 	{
 		hap += count;
-		printf("%02d±îÁö ´©ÀûÇÕ Ãâ·Â : hap = %02d\n", count, hap);
+		printf("%02dê¹Œì§€ ëˆ„ì í•© ì¶œë ¥ : hap = %02d\n", count, hap);
 	}
 	printf("1+2+~+9+10 = %02d\n", hap);
 	return 0;
