47205a821d63442fa6dab91ca229fda954a2dd29
qkrtnqlspsb <bp1211@naver.com>
2023-09-14 16:25:34 +0900

    lecture10

---
+++ Project2/lecture9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int count, hap = 0;
+
+	count = 1;
+	while (count <= 10)
+	{
+		hap += count;
+		printf("hap=%d, count=%d\n", hap, count);
+		count++;
+	}
+	printf("%d\n", hap);
+	return 0;
+}
